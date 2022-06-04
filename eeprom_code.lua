2ad9fe3c9aec816a10ce6894c0cfba2a9cfdbe24
Oyo-Player <86727463+Oyo-Player@users.noreply.github.com>
2022-06-04 21:25:41 +0200

    Add files via upload

---
+++ eeprom_code.lua
@@ -0,0 +1,55 @@
+
+function component.get(comp_name)
+  local comp = component.list(comp_name)()
+
+  if comp then return comp end
+  return nil
+end
+
+function boot_invoke(adress, method, ...)
+  local result = table.pack(pcall(component.invoke, adress, method, ...))
+  if not result[1] then
+    return nil, result[2]
+  else
+    return table.unpack(result, 2, result.n)
+  end
+end
+
+local gpu = component.get("gpu")
+local screen = component.get("screen")
+local state = 0
+local text = "☺ You are an idiot! ☺"
+
+if not gpu or not screen then error("You are an idiot!") end
+boot_invoke(gpu, "bind", screen)
+
+function Sleep(timeout)
+  local deadline = computer.uptime() + timeout
+  repeat
+    computer.pullSignal(deadline - computer.uptime())
+  until computer.uptime() >= deadline
+end
+
+function Clear()
+  local resx,resy = boot_invoke(gpu, "getResolution")
+  boot_invoke(gpu, "fill", 1,1, resx,resy, " ")
+end
+
+while true do
+  local resx,resy = boot_invoke(gpu, "getResolution")
+
+  if state == 0 then
+    boot_invoke(gpu, "setBackground", 0x000000)
+    boot_invoke(gpu, "setForeground", 0xffffff)
+    state = 1
+  else
+    boot_invoke(gpu, "setBackground", 0xffffff)
+    boot_invoke(gpu, "setForeground", 0x000000)
+    state = 0
+  end
+
+  Clear()
+  boot_invoke(gpu, "set", math.floor((resx - #text / 2) / 2), math.floor(resy / 2), text)
+
+  Sleep(1)
+end
