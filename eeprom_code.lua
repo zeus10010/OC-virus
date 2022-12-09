8d9ddfa9fdaf5395c11d21c6676ca2d262c5e36d
Oyo-Player <86727463+Oyo-Player@users.noreply.github.com>
2022-12-09 23:01:20 +0100

    Update eeprom_code.lua

--- eeprom_code.lua
+++ eeprom_code.lua
@@ -1,27 +1,13 @@
-function component.get(comp_name)
-  local comp = component.list(comp_name)()
+local invoke = component.invoke
 
-  if comp then return comp end
-  return nil
-end
-
-function boot_invoke(adress, method, ...)
-  local result = table.pack(pcall(component.invoke, adress, method, ...))
-  if not result[1] then
-    return nil, result[2]
-  else
-    return table.unpack(result, 2, result.n)
-  end
-end
-
-local gpu = component.get("gpu")
-local screen = component.get("screen")
+local gpu = component.list("gpu")()
+local screen = component.list("screen")()
 local state = 0
 local text = "You are an idiot"
 
 if not gpu or not screen then error("You are an idiot") end
-boot_invoke(gpu, "bind", screen)
-boot_invoke(gpu, "setResolution", 50,16)
+invoke(gpu, "bind", screen)
+invoke(gpu, "setResolution", 50,16)
 
 local resx, resy = boot_invoke(gpu, "getResolution")
 local startx = (resx / 2 - #text / 2) + 1
@@ -36,17 +22,17 @@ end
 
 while true do
   if state == 0 then
-    boot_invoke(gpu, "setBackground", 0x000000)
-    boot_invoke(gpu, "setForeground", 0xffffff)
+    invoke(gpu, "setBackground", 0x000000)
+    invoke(gpu, "setForeground", 0xffffff)
     state = 1
   else
-    boot_invoke(gpu, "setBackground", 0xffffff)
-    boot_invoke(gpu, "setForeground", 0x000000)
+    invoke(gpu, "setBackground", 0xffffff)
+    invoke(gpu, "setForeground", 0x000000)
     state = 0
   end
 
-  boot_invoke(gpu, "fill", 1,1, resx,resy, " ")
-  boot_invoke(gpu, "set", startx, starty, text)
+  invoke(gpu, "fill", 1,1, resx,resy, " ")
+  invoke(gpu, "set", startx, starty, text)
   computer.beep()
 
   Sleep(1)
